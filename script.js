2172c1d14b639e1eabffa0186e9c5dcbb4a56c68
Willians Martins <professorwillians@gmail.com>
2020-01-20 20:05:41 -0300

    randomizar

--- script.js
+++ script.js
@@ -1,10 +1,45 @@
 $(document).ready(function(){
     //Script.colorir();
-    Script.metodo();
+    Script.apresentarImagem();
+    // Script.cursiva();
+    // Script.randomize();
+    Script.clicks();
 });
 
 
 Script = {
+
+    shuffleAds: function(arr){
+        for(var j, x, i = arr.length; i; j = parseInt(Math.random() * i), x = arr[--i], arr[i] = arr[j], arr[j] = x);
+        return arr;
+    },
+
+    randomize: function(){
+        // var alfabeto = Script.random('a', 'z');
+        // alfabeto.forEach(element => {
+        //     console.info("-->" + element);
+        // });
+
+        var letras = $(".letra");
+        Script.shuffleAds(letras);
+        
+        // $(".letras").empty();
+        
+        $(".letra").each(function(chave, valor){
+            console.info("-->" + chave);
+            console.info("-->" + valor);
+            // $(".letras").append(valor);
+            $(valor).remove();
+        });
+        
+        $(".letras").append(letras);
+        Script.apresentarImagem();
+    },
+
+    random: function(c1, c2) {
+        a = 'abcdefghijklmnopqrstuvwxyz'.split('');
+        return (a.slice(a.indexOf(c1), a.indexOf(c2) + 1)); 
+    },
 
 	colorir: function(){
 		$(".letra").click(function(){
@@ -12,11 +47,33 @@ Script = {
         });
     },
 
-    metodo: function(){
+    apresentarImagem: function(){
 		$(".letra").click(function(){
-            $(this).find("span").hide();
+            $(this).find("span").toggle();
             var imagem = $(this).find("img");
-            $(imagem).show();
+            $(imagem).toggle();
+        });
+    },
+
+    clicks: function(){
+        $(".btn-cursiva").click(function(){
+            $(".letra span").css('font-family', 'cursive');
+        });
+
+        $(".btn-maiuscula").click(function(){
+            $(".letra span").css('text-transform', 'uppercase');
+        });
+
+        $(".btn-minuscula").click(function(){
+            $(".letra span").css('text-transform', 'lowercase');
+        });
+
+        $(".btn-forma").click(function(){
+            $(".letra span").css('font-family', 'arial');
+        });
+
+        $(".btn-randomize").click(function(){
+            Script.randomize();
         });
     }
 }
